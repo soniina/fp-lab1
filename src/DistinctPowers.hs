89499da3d6d5d8409908b113ba14395e55001f49
soniina <sonikpav@gmail.com>
2025-10-06 13:50:11 +0300

    feat: implement solutions for problem #29

---
+++ src/DistinctPowers.hs
@@ -0,0 +1,32 @@
+module DistinctPowers (recSol, tailRecSol, moduleSol, mapSol, infiniteListSol) where
+
+import Data.List (nub)
+import qualified Data.Set as Set
+
+recSol :: Integer -> Int
+recSol n = length (nub (helper 2))
+  where
+    helper a
+      | a > n = []
+      | otherwise = map (a ^) [2 .. n] ++ helper (a + 1)
+
+tailRecSol :: Integer -> Int
+tailRecSol n = helper 2 Set.empty
+  where
+    helper a acc
+      | a > n = Set.size acc
+      | otherwise = helper (a + 1) (Set.union acc (Set.fromList [a ^ b | b <- [2 .. n]]))
+
+moduleSol :: Integer -> Int
+moduleSol n = count
+  where
+    generate = [(a, b) | a <- [2 .. n], b <- [2 .. n]]
+    powerPairs = map (uncurry (^)) generate
+    uniqueSet = Set.fromList powerPairs
+    count = Set.size uniqueSet
+
+mapSol :: Integer -> Int
+mapSol n = Set.size $ Set.fromList $ concatMap (\a -> map (a ^) [2 .. n]) [2 .. n]
+
+infiniteListSol :: Integer -> Int
+infiniteListSol n = Set.size . Set.fromList . takeWhile (<= n ^ n) $ concatMap (\a -> map (a ^) [2 .. n]) [2 ..]
