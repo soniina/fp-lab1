0ae3628726bd1cba143111430411ab9b528942d7
soniina <sonikpav@gmail.com>
2025-10-06 13:32:41 +0300

    feat: implement solutions for problem #2

---
+++ src/FibSumEven.hs
@@ -0,0 +1,38 @@
+module FibSumEven (recSol, tailRecSol, moduleSol, mapSol, infiniteListSol)  where
+
+recSol :: Int -> Int -> Int
+recSol a b
+  | next > 4000000 = 0
+  | even next = next + recSol b next
+  | otherwise = recSol b next
+  where
+    next = a + b
+
+tailRecSol :: Int -> Int -> Int
+tailRecSol a b = helper a b 0
+  where
+    helper x y acc
+      | next > 4000000 = acc
+      | even next = helper y next (acc + next)
+      | otherwise = helper y next acc
+      where
+        next = x + y
+
+moduleSol :: Int -> Int -> Int
+moduleSol a b = moduleSum . moduleEven $ moduleFibSeq a b
+  where
+    moduleSum = foldl (+) 0
+    moduleEven = filter even
+    moduleFibSeq x y = takeWhile (<= 4000000) fibs
+      where
+        fibs = x : y : zipWith (+) fibs (tail fibs)
+
+mapSol :: Int -> Int -> Int
+mapSol a b = sum $ filter even fibs
+  where
+    fibs = takeWhile (<= 4000000) $ map fst $ iterate (\(x, y) -> (y, x + y)) (a, b)
+
+infiniteListSol :: Int -> Int -> Int
+infiniteListSol a b = sum $ filter even $ takeWhile (<= 4000000) fibs
+  where
+    fibs = a : b : zipWith (+) fibs (tail fibs)
